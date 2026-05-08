2dc27be9e250349124d1571538193852c9ec12b6
Tejaswis <Tejaswis489@gmail.com>
2026-05-08 15:00:56 +0530

    string pro added

---
+++ src/string/rev_string_forloop.java
@@ -0,0 +1,16 @@
+package string;
+
+public class rev_string_forloop {
+    public static void main(String [] args)
+    {
+
+        String str = "Teju";
+        String rev = " ";
+
+        for(int i = str.length() - 1 ; i>=0 ; i--)
+        {
+            rev = rev + str.charAt(i);
+        }
+        System.out.println(rev);
+    }
+}
