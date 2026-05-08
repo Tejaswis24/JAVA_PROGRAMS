605cb7eb38c09d0a08c6583164e5795ba536698e
tejaswi <tejaswis489@gmail.com>
2026-05-08 17:26:24 +0530

    update

--- src/string/ReverseStringAppend.java
+++ src/string/ReverseStringAppend.java
@@ -1,4 +1,18 @@
 package string;
 
 public class ReverseStringAppend {
+
+    public static void main(String[] args) {
+
+        String str = "Hello";
+        StringBuilder reverse = new StringBuilder();
+
+        // Reverse string using append()
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse.append(str.charAt(i));
+        }
+
+        System.out.println("Reversed String: " + reverse);
+    }
+
 }
