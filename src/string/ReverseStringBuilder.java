7cfb074fd0421a272e97cadf058717a259583469
Tejaswis <Tejaswis489@gmail.com>
2026-05-08 15:17:41 +0530

    stringbuilder

---
+++ src/string/ReverseStringBuilder.java
@@ -0,0 +1,17 @@
+package string;
+
+public class ReverseStringBuilder {
+    public static void main(String[] args) {
+
+        String str = "Hello";
+
+        // Using StringBuilder
+        StringBuilder sb = new StringBuilder(str);
+
+        // Reverse the string
+        sb.reverse();
+
+        // Print reversed string
+        System.out.println("Reversed String: " + sb);
+    }
+}
