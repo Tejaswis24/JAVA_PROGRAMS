37de25d05d4e3f1a14069d84d52d3566af1d9383
Tejaswis <Tejaswis489@gmail.com>
2026-05-08 15:28:08 +0530

    buffer

---
+++ src/string/ReverseStringBuffer.java
@@ -0,0 +1,16 @@
+package string;
+
+public class ReverseStringBuffer {
+    public static void main(String[] args) {
+        String original = "Hello World";
+
+        // Create StringBuffer object
+        StringBuffer sb = new StringBuffer(original);
+
+        // Reverse the string
+        sb.reverse();
+
+        // Print reversed string
+        System.out.println("Reversed string: " + sb);
+    }
+}
