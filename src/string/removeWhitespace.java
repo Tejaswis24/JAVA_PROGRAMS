cd99b9710c3eae1761f2b4cc230bf89b42225649
tejaswi <tejaswis489@gmail.com>
2026-05-11 15:12:16 +0530

    cherrypick

--- src/string/removeWhitespace.java
+++ src/string/removeWhitespace.java
@@ -5,7 +5,9 @@ public class removeWhitespace
     public static void main (String [] args)
     {
         String str = " Hello World ";
-        String res = str.replaceAll(" ", "");
+         String res=str.replaceAll(" ", "");
+        System.out.println(res);
         System.out.println(res);
     }
+
 }
